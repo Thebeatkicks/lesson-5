0f5c197e26e2931236deb1ab4e99990d280f8e5b
Thebeatkicks <000henrik@gmail.com>
2025-04-02 10:14:54 +0200

    all functions is created. Need to creat some kind of loop so that we can get back to atm() and start over untill 5 if chosen.

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 const account = {
     accountName: "Henrik Martini",
     balance: 2,
@@ -22,13 +20,15 @@ function atm() {
             withdrawal();
             break
         case 4:
-            getAccountName()
+            getAccountName();
             break
         case 5:
+            exitAccount();
+            break
     }
 }
 
- function getBalance() {
+function getBalance() {
     alert("You have: " + account.balance + "kr, on your account.");
 }
 
@@ -41,8 +41,8 @@ function deposit() {
 function withdrawal() {
     let removeMoney = parseFloat(prompt("How much would you like to withdraw from your account?"))
     account["balance"] = account.balance - removeMoney;
-    console.log(account.balance)
-    //alert("You have withdrawn: " + removeMoney + "kr! You now have " + account.balance + "kr, on your account!" );
+    //console.log(account.balance)
+    alert("You have withdrawn: " + removeMoney + "kr! You now have " + account.balance + "kr, on your account!" );
 }
 
 function getAccountName() {
@@ -58,75 +58,3 @@ function exitAccount() {
 
 atm();
 
-/*
- - deposit, also a function 
-
- this function should be able to deposit money onto the balance of the account 
-
- 
-
- - withdrawal, also a function 
-
- this function should be able do withdraw money from the balance of the account 
-
- 
-
- - getAccountName, function as well 
-
- this function should display the account holders name to the user 
-
-
- - accountError, same as above function! 
-
-
- this one is a bit tricky... it's up to you to figure out how or what you should use this for. 
-
-HINT: it's more a thinking poblem than a technical problem :) 
-
- 
-
-EXTRA: exitAccount, should be a function 
-
-this function should exit the account 
-
-HINT: there are a few different ways to do this, it's up to you which way you choose. 
-
-
-EXTRA = OPTIONAL NOT MANDATORY 
-
- 
-
-Remember that a function is just a value. And if a function is just a 
-
-value then we can both pass it as a parameter to a function and 
-
-pass it as a property of an object. 
-
- 
-
-The object should handle all of the functionality (logic) 
-
- 
-
-The atm() function should be responsible for showing the user interface 
-
-and based on the user input show the right meny choice 
-
-I have created some starting code for you: 
-
-En bild som visar skärmbild, text
-
-Automatiskt genererad beskrivning 
-
- */
-
-/*
-1. create an object
-2. prompt questions to give data to the object
-3. when all questions are answered prompt atm function
-4. code all atm choises
-4. add an error? if removeMoney > account.balance, return to atm()
-
-switch/case
-
-*/
